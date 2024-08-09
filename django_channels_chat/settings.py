c3e045f54e581e507d2072d43b20c0b2148bfeab
Lana <lan2828@yandex.ru>
2024-08-09 10:16:08 +0300

    chat app extended, sockets set up

--- django_channels_chat/settings.py
+++ django_channels_chat/settings.py
@@ -122,3 +122,9 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels.layers.InMemoryChannelLayer"
+    }
+}
